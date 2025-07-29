98aa545cdbb96564bd9c85060794030e87fe1f65
Peace-pluto <munyaopeace34@gmail.com>
2025-07-29 16:40:17 +0300

    Initial commit

---
+++ simple_calculator.py
@@ -0,0 +1,24 @@
+# Simple calculator program
+
+# Ask user for input
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+# Perform operation
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Division by zero"
+else:
+    result = "Invalid operation"
+
+# Print result
+print("Result:", result)
